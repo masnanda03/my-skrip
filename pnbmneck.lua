5c5d3454df08855b33f6d7bb06b05f1c6a15d9dd
masnanda03 <159537467+masnanda03@users.noreply.github.com>
2024-06-06 20:32:59 +0700

    Update pnbmneck.lua

--- pnbmneck.lua
+++ pnbmneck.lua
@@ -340,21 +340,6 @@ end
 end
 GetMagN()
 
-local function GetTel()
-Tel = {}
-Pos = 0
-for x = 0,199 do
-for y = 0,199 do
-tile = GetTile(x, y)
-if (tile.fg == 3898) then
-Pos = Pos + 1
-Tel[Pos] = {x = tile.x, y = tile.y}
-end
-end
-end
-end
-GetTel()
-
 local function scheat()
     if (cheats == true) and (GetLocal().pos.x//32 == xawal) and (GetLocal().pos.y//32 == yawal) then
 if MneckON == false then
