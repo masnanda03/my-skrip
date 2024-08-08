f1002c63cc18a6426da91f16403290fa837969ff
masnanda03 <159537467+masnanda03@users.noreply.github.com>
2024-08-08 18:26:17 +0700

    Update pnbmneck.lua

--- pnbmneck.lua
+++ pnbmneck.lua
@@ -568,6 +568,7 @@ for _, id in pairs(tabel_uid) do
     end
 end
 
+DetachConsole()
 if match_found == true then
     LogToConsole("`0[`^MUFFINN`0-`^STORE`0] `^IDENTIFY PLAYER : " .. GetLocal().name)
     Sleep(1000)
