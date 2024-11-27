8a1387687df94b8e8f8249df02b2547717702693
masnanda03 <159537467+masnanda03@users.noreply.github.com>
2024-11-27 20:24:04 +0700

    Add user ID 837096 to AUTO MAKE MCHEM

--- mchemicals.lua
+++ mchemicals.lua
@@ -1,5 +1,5 @@
 -- [ MAKE MCHEMICALS - MUFFINN COMMUNITY ] --
-tabel_uid = {"134611", "911"}
+tabel_uid = {"134611", "911", "837096"}
 
 -- [NO TOUCH BICES] --
 -- Chemical IDs and Recipe Requirements
